11c7aa2e844d1ea047945d95b2e5d11ec87d636c
Axoh24 <84033769+Axoh24@users.noreply.github.com>
2021-05-16 19:36:03 +0200

    Add files via upload

---
+++ ProgramingIsFun.java
@@ -0,0 +1,13 @@
+package assignment2;
+
+import javax.swing.JOptionPane;
+
+public class ProgramingIsFun {
+	
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null,"Programing is fun!");
+		
+		System.exit(0);
+	}
+}
